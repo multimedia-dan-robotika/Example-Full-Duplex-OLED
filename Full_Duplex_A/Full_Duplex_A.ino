79346b52c1b47f6111ab51a47a4fc5d710d3a001
Ardhiansyah Hadi <70263752+Ardhiansyah08@users.noreply.github.com>
2022-09-27 14:12:39 +0700

    First Upload

---
+++ Full_Duplex_A/Full_Duplex_A.ino
@@ -0,0 +1,144 @@
+#include <SPI.h>
+#include <LoRa.h>
+#include <Wire.h>
+#include <Adafruit_GFX.h>
+#include <Adafruit_SSD1306.h>
+
+#define SCK 5
+#define MISO 19
+#define MOSI 27
+#define SS 18
+#define RST 14
+#define DIO0 26
+
+#define OLED_SDA 21
+#define OLED_SCL 22 
+#define OLED_RST 16
+#define SCREEN_WIDTH 128 // OLED display width, in pixels
+#define SCREEN_HEIGHT 64 // OLED display height, in pixels
+
+byte localAddress = 0xAA;
+byte destinationAddress = 0xBB;
+
+long lastSendTime = 0;
+int interval = 2000;
+int count = 0;
+
+Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RST);
+
+void setup() {
+  Serial.begin(9600);
+  
+  //reset OLED display via software
+  pinMode(OLED_RST, OUTPUT);
+  digitalWrite(OLED_RST, LOW);
+  delay(20);
+  digitalWrite(OLED_RST, HIGH);
+
+  //initialize OLED
+  Wire.begin(OLED_SDA, OLED_SCL);
+  if(!display.begin(SSD1306_SWITCHCAPVCC, 0x3c, false, false)) { // Address 0x3C for 128x32
+    Serial.println(F("SSD1306 allocation failed"));
+    for(;;); // Don't proceed, loop forever
+  }
+  
+  display.clearDisplay();
+  display.setTextColor(WHITE);
+  display.setTextSize(1);
+  display.setCursor(0,0);
+  display.print("LORA DUPLEX ");
+  display.display();
+  
+  Serial.println("Start LoRa DUplex");
+
+  //SPI LoRa pins
+  SPI.begin(SCK, MISO, MOSI, SS);
+  //setup LoRa transceiver module
+  LoRa.setPins(SS, RST, DIO0);
+  
+  if (!LoRa.begin(433E6)) {
+    Serial.println("Starting LoRa failed!");
+    while (1);
+  }
+  Serial.println("LoRa Initializing OK!");
+  display.setCursor(0,10);
+  display.print("LoRa Initializing OK!");
+  display.display();
+  delay(2000);
+}
+
+void loop() {
+  if (millis() - lastSendTime > interval) {
+    String sensorData = String(count++);
+    sendMessage(sensorData);
+
+    Serial.print("Sending data " + sensorData);
+    Serial.print(" from 0x" + String(localAddress, HEX));
+    Serial.println(" to 0x" + String(destinationAddress, HEX));
+    
+  display.clearDisplay();
+  display.setCursor(0,0);
+  display.println("LoRa Duplex A");
+  display.setCursor(0,20);
+  display.setTextSize(1);
+  display.print("Sending data" + sensorData);
+  display.setCursor(0,30);
+  display.print(" from 0x" + String(localAddress, HEX));
+  display.print(" to 0x" + String(destinationAddress, HEX));
+  display.setCursor(50,30);     
+  display.display();
+
+    lastSendTime = millis();
+    interval = random(2000) + 1000;
+  }
+
+  receiveMessage(LoRa.parsePacket());
+}
+
+void sendMessage(String outgoing) {
+  LoRa.beginPacket();
+  LoRa.write(destinationAddress);
+  LoRa.write(localAddress);
+  LoRa.write(outgoing.length());
+  LoRa.print(outgoing);
+  LoRa.endPacket();
+}
+
+void receiveMessage(int packetSize) {
+  if (packetSize == 0) return;
+
+  int recipient = LoRa.read();
+  byte sender = LoRa.read();
+  byte incomingLength = LoRa.read();
+
+  String incoming = "";
+
+  while (LoRa.available()) {
+    incoming += (char)LoRa.read();
+  }
+
+  if (incomingLength != incoming.length()) {
+    Serial.println("Error: Message length does not match length");
+    return;
+  }
+
+  if (recipient != localAddress) {
+    Serial.println("Error: Recipient address does not match local address");
+    return;
+  }
+
+  Serial.print("Received data " + incoming);
+  Serial.print(" from 0x" + String(sender, HEX));
+  Serial.println(" to 0x" + String(recipient, HEX));
+  
+  display.clearDisplay();
+  display.setCursor(0,0);
+  display.println("LoRa Duplex A");
+  display.setCursor(0,40);
+  display.setTextSize(1);
+  display.print("Received data" + incoming);
+  display.setCursor(0,50);
+  display.print(" from 0x" + String(sender, HEX));
+  display.print(" to 0x" + String(recipient, HEX));  
+  display.display();
+}
